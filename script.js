2bd00591ef53e740d3cbfe802828f32a1cbeffc8
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-20 10:39:34 -0600

    delete old code as math did not create accurate grids. attempt retry with grid layout.

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-//grid set up
-const gridContainer = document.querySelector("#canvasWrap");
-let cellCount = 30;
-const area = 25000;
+window.addEventListener("load", () => {
+  let canvas = document.querySelector("#canvasWrap");
 
-function columnNum() {
-  let cellArea = area / cellCount;
-  let column = Math.ceil(Math.sqrt(cellArea));
-  return column;
-}
-
-const gridBase = document.createElement("div");
-
-for (let i = 0; i < cellCount; i++) {
-  let cell = document.createElement("div");
-  cell.classList.add("cell");
-  gridContainer.appendChild(cell);
-}
-
-console.log(columnNum());
+  canvas.style.gridTemplateColumns = "repeat(16, 1fr)";
+  canvas.style.gridTemplateRows = "repeat(16, 1fr)";
+});
