b16332df4f8f0ae14f42965bcb7aed83e8125c9d
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-20 12:10:01 -0600

    add input field and submit button for future canvas grid customization

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-window.addEventListener("load", () => {
-  let canvas = document.querySelector("#canvasWrap");
+const inpValue = document.querySelector("#canvasValue");
+const inpButton = document.querySelector("#inpButton");
+const canvas = document.querySelector("#canvasWrap");
+inpValue.defaultValue = 16;
+gridInput = inpValue.value;
 
-  canvas.style.gridTemplateColumns = "repeat(16, 1fr)";
-  canvas.style.gridTemplateRows = "repeat(16, 1fr)";
-});
+canvas.style.gridTemplateColumns = `repeat(${gridInput}, 1fr)`;
+canvas.style.gridTemplateRows = `repeat(${gridInput}, 1fr)`;
+
+function changeInput() {}
+
+inpButton.addEventListener("click", changeInput);
