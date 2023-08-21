11b77a663dbf56f03999179a859cf640169e5d80
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-20 18:23:05 -0600

    adjust screen resolution to be bigger

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ window.addEventListener("load", () => {
   setCanvas(16);
 });
 
+//variables
 const inpValue = document.querySelector("#canvasValue");
 const inpButton = document.querySelector("#inpButton");
 const canvas = document.querySelector("#canvasWrap");
-const blkButton = document.getElementById("blkButton");
+const clrPicker = document.getElementById("clrPicker");
 const rnbButton = document.getElementById("rnbButton");
 const clrButton = document.getElementById("clrButton");
 inpValue.defaultValue = 16;
 
 function setCanvas(gridInput) {
-  side = Math.ceil(Math.sqrt(gridInput));
+  side = Math.ceil(gridInput);
   area = side * side;
 
   canvas.style.gridTemplateColumns = `repeat(${side}, 1fr)`;
@@ -20,10 +21,14 @@ function setCanvas(gridInput) {
   for (let i = 0; i < area; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
     canvas.appendChild(cell);
   }
 }
 
+//accepts client input to change canvas size
 inpButton.addEventListener("click", () => {
   let newInput = inpValue.value;
   if (newInput <= 0) {
@@ -39,8 +44,11 @@ inpButton.addEventListener("click", () => {
   }
 });
 
+//clear canvas when page reload/size change; stops overflow;
 function clearCanvas(container) {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
+
+//color handling
